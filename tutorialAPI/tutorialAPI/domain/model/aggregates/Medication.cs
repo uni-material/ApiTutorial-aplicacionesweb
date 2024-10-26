53bf086edb442cfd620646fef42cb72ef10136e8
Ro <u202213765@upc.edu.pe>
2024-10-26 16:53:00 -0500

    feat: add medication aggregate.

---
+++ tutorialAPI/tutorialAPI/domain/model/aggregates/Medication.cs
@@ -0,0 +1,15 @@
+﻿namespace tutorialAPI.tutorialAPI.domain.model.aggregates;
+
+public class Medication
+{
+    public string id { get;}
+    public string name { get; private set; }
+    public string description { get; private set; }
+    public string quantity { get; private set; }
+    public string frecuency { get; private set; }
+
+    public Medication()
+    {
+        
+    }
+}
