070e9569497a70801d02e1082ec39dc6efa50d00
Ro <u202213765@upc.edu.pe>
2024-10-26 21:28:56 -0500

    chore: add medication repository.

---
+++ tutorialAPI/tutorialAPI/domain/repositories/IMedicationRepository.cs
@@ -0,0 +1,12 @@
+﻿using tutorialAPI.tutorialAPI.domain.model.aggregates;
+
+namespace tutorialAPI.tutorialAPI.domain.repositories;
+
+public interface IMedicationRepository
+{
+    Task<IEnumerable<Medication>> GetAllMedications();
+    Task<Medication> GetDetails(int id);
+    Task<bool> InsertMedication(Medication medication);
+    Task<bool> UpdateMedication(Medication medication);
+    Task<bool> DeleteMedication(Medication medication);
+}
