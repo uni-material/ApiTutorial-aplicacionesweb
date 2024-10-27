2172d6f7643714a36d0ad46cd44ad9a3900b53cf
Ro <u202213765@upc.edu.pe>
2024-10-26 21:28:41 -0500

    chore: add medication controller.

---
+++ tutorialAPI/tutorialAPI/Controllers/MedicationController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tutorialAPI.tutorialAPI.domain.repositories;
+
+namespace tutorialAPI.tutorialAPI.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+
+public class MedicationController : ControllerBase
+{
+
+    private readonly IMedicationRepository _medicationRepository;
+
+    public MedicationController(IMedicationRepository medicationRepository)
+    {
+        _medicationRepository = medicationRepository;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllMedications()
+    {
+        return Ok(await _medicationRepository.GetAllMedications());
+    }
+    
+    
+    
+    //[HttpPost]
+    //[Route("save")]
+    //public dynamic saceMedication()
+    //{
+    //    
+    //}
+
+
+
+}
